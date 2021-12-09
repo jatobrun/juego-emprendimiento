94f7db3de83a431a98dfd5b63304362f72c5afa1
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2021-12-09 18:34:36 -0500

    print statements

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -30,6 +30,8 @@ def login():
     response = {}
     data = request.data
     data_dictionary = json.loads(data)
+    print(data)
+    print(data_dictionary)
     try:
         usuario = Usuarios.query.filter(Usuarios.usuario == data_dictionary["usuario"]).first()
         contraseña = data_dictionary['contraseña']
@@ -58,6 +60,8 @@ def crear_usuario():
     error = False
     data = request.data
     data_dictionary = json.loads(data)
+    print(data)
+    print(data_dictionary)
     cedula = data_dictionary["cedula"]
     nombre = data_dictionary["nombre"]
     apellido = data_dictionary["apellido"]
@@ -104,6 +108,8 @@ def crear_canje():
     error = False
     data = request.data
     data_dictionary = json.loads(data)
+    print(data)
+    print(data_dictionary)
     usuario_id = data_dictionary['usuario_id']
     total_puntos = data_dictionary['total_puntos']
     cantidad_total = data_dictionary['cantidad_total']
@@ -174,6 +180,8 @@ def game_over():
     error = False
     data = request.data
     data_dictionary = json.loads(data)
+    print(data)
+    print(data_dictionary)
     usuario_id = data_dictionary["usuario_id"]
     puntos = data_dictionary["puntos"]
 
